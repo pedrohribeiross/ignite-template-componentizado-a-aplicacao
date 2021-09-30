c004a9ffdb2032aa1d97b76712c9ab27ffb69238
Pedro Henrique Ribeiro <pedro_henrique.rss@hotmail.com>
2021-09-30 09:57:03 -0300

    Created movie genre selection context.

---
+++ src/contexts/GenreContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useState } from 'react';
+
+type GenreContextType = {
+	selectedGenreId: number;
+	handleClickButton: (id: number) => void;
+};
+
+type GenreContextProviderProps = {
+	children: ReactNode;
+};
+
+export const GenreContext = createContext({} as GenreContextType);
+
+export function GenreContextProvider(props: GenreContextProviderProps) {
+	const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+	function handleClickButton(id: number) {
+		setSelectedGenreId(id);
+	}
+
+	return (
+		<GenreContext.Provider value={{ selectedGenreId, handleClickButton }}>
+			{props.children}
+		</GenreContext.Provider>
+	);
+}
